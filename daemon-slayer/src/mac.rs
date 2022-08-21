f033f2ac555237c9945f4be49ebdb1a139f637f9
Austin Schey <aschey13@gmail.com>
2022-08-21 17:51:11 -0500

    fix mac build

--- daemon-slayer/src/mac.rs
+++ daemon-slayer/src/mac.rs
@@ -1,13 +1,14 @@
 use std::{
-    env::current_exe,
-    fs, iter,
+    fs,
     path::PathBuf,
     process::{Command, Stdio},
 };
 
 use launchd::Launchd;
 
-use crate::service_status::ServiceStatus;
+use crate::{
+    service_config::ServiceConfig, service_manager::ServiceManager, service_status::ServiceStatus,
+};
 
 #[macro_export]
 macro_rules! define_service {
@@ -41,7 +42,7 @@ macro_rules! define_service {
                     }
                 });
 
-                $service_main_func(vec![], sender, receiver)
+                $service_main_func(sender, receiver)
             }
 
             pub fn $service_func_name()  {
@@ -52,51 +53,64 @@ macro_rules! define_service {
 }
 
 pub struct Manager {
-    service_name: String,
+    config: ServiceConfig,
 }
 
 impl Manager {
-    pub fn new<T: Into<String>>(service_name: T) -> Self {
-        Self {
-            service_name: service_name.into(),
+    fn run_launchctl(&self, args: Vec<&str>) -> String {
+        let output = Command::new("launchctl")
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .args(args)
+            .output()
+            .unwrap();
+
+        if output.status.success() {
+            String::from_utf8(output.stdout).unwrap()
+        } else {
+            String::from_utf8(output.stderr).unwrap()
         }
     }
 
-    pub fn install<T: Into<String>>(&self, args: Vec<T>) {
-        let service_binary_path = current_exe().unwrap();
-        let file = Launchd::new(&self.service_name, &service_binary_path)
+    fn get_plist_path(&self) -> PathBuf {
+        PathBuf::from("/Library/LaunchDaemons").join(format!("{}.plist", self.config.name))
+    }
+}
+
+impl ServiceManager for Manager {
+    fn new(config: ServiceConfig) -> Self {
+        Self { config }
+    }
+
+    fn install(&self) {
+        let file = Launchd::new(&self.config.name, &self.config.program)
             .unwrap()
-            .with_program_arguments(
-                iter::once(service_binary_path.to_string_lossy().to_string())
-                    .chain(args.into_iter().map(|a| a.into()))
-                    .collect(),
-            );
-        let path =
-            PathBuf::from("/Library/LaunchDaemons").join(format!("{}.plist", self.service_name));
-        file.to_writer_xml(std::fs::File::create(path).unwrap())
+            .with_program_arguments(self.config.args_iter().map(|a| a.to_owned()).collect());
+
+        let path = self.get_plist_path();
+        file.to_writer_xml(std::fs::File::create(&path).unwrap())
             .unwrap();
-        let path =
-            PathBuf::from("/Library/LaunchDaemons").join(format!("{}.plist", self.service_name));
+
         self.run_launchctl(vec!["load", &path.to_string_lossy()]);
     }
 
-    pub fn uninstall(&self) {
-        let path =
-            PathBuf::from("/Library/LaunchDaemons").join(format!("{}.plist", self.service_name));
+    fn uninstall(&self) {
+        let path = self.get_plist_path();
         self.run_launchctl(vec!["unload", &path.to_string_lossy()]);
         fs::remove_file(path).unwrap();
     }
 
-    pub fn start(&self) {
-        self.run_launchctl(vec!["start", &self.service_name]);
+    fn start(&self) {
+        self.run_launchctl(vec!["start", &self.config.name]);
     }
 
-    pub fn stop(&self) {
-        self.run_launchctl(vec!["stop", &self.service_name]);
+    fn stop(&self) {
+        self.run_launchctl(vec!["stop", &self.config.name]);
     }
 
-    pub fn query_status(&self) -> ServiceStatus {
-        let output = self.run_launchctl(vec!["print", &format!("system/{}", self.service_name)]);
+    fn query_status(&self) -> ServiceStatus {
+        let output = self.run_launchctl(vec!["print", &format!("system/{}", self.config.name)]);
         if output.starts_with("Could not find service") {
             return ServiceStatus::NotInstalled;
         }
@@ -117,22 +131,6 @@ impl Manager {
             ServiceStatus::Started
         } else {
             ServiceStatus::Stopped
-        }
-    }
-
-    fn run_launchctl(&self, args: Vec<&str>) -> String {
-        let output = Command::new("launchctl")
-            .stdin(Stdio::null())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .args(args)
-            .output()
-            .unwrap();
-
-        if output.status.success() {
-            String::from_utf8(output.stdout).unwrap()
-        } else {
-            String::from_utf8(output.stderr).unwrap()
         }
     }
 }
