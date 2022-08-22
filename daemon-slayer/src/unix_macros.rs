ccd963b7ef9159f7ea07e4b2c1cad8da03c5011f
Austin Schey <aschey13@gmail.com>
2022-08-21 19:34:58 -0500

    Revert "refactor macro"
    This reverts commit 27926bbb65f65c2add4ed71afdcbf6faf9ba8262.

--- daemon-slayer/src/unix_macros.rs
+++ daemon-slayer/src/unix_macros.rs
@@ -1,71 +1,66 @@
 #[macro_export]
 macro_rules! define_service {
-    (@tokio_signal_handler $on_stop:expr) => {
-        $crate::tokio::spawn(async move {
-            use $crate::futures::stream::StreamExt;
-
-            let signals = $crate::signal_hook_tokio::Signals::new(&[
-                $crate::signal_hook::consts::signal::SIGHUP,
-                $crate::signal_hook::consts::signal::SIGTERM,
-                $crate::signal_hook::consts::signal::SIGINT,
-                $crate::signal_hook::consts::signal::SIGQUIT,
-            ])
-            .unwrap();
-
-            //let handle = signals.handle();
-
-            let mut signals = signals.fuse();
-            while let Some(signal) = signals.next().await {
-                match signal {
-                    $crate::signal_hook::consts::signal::SIGTERM
-                    | $crate::signal_hook::consts::signal::SIGINT
-                    | $crate::signal_hook::consts::signal::SIGQUIT
-                    | $crate::signal_hook::consts::signal::SIGHUP => {
-                        $on_stop;
-                    }
-                    _ => {}
-                }
-            }
-        });
-    };
-
-    (@std_signal_handler $on_stop:expr) => {
-        std::thread::spawn(move || {
-            let mut signals = $crate::signal_hook::iterator::Signals::new(&[
-                $crate::signal_hook::consts::signal::SIGHUP,
-                $crate::signal_hook::consts::signal::SIGTERM,
-                $crate::signal_hook::consts::signal::SIGINT,
-                $crate::signal_hook::consts::signal::SIGQUIT,
-            ])
-            .unwrap();
-
-            for signal in &mut signals {
-                match signal {
-                    $crate::signal_hook::consts::signal::SIGTERM
-                    | $crate::signal_hook::consts::signal::SIGINT
-                    | $crate::signal_hook::consts::signal::SIGQUIT
-                    | $crate::signal_hook::consts::signal::SIGHUP => {
-                        $on_stop;
-                    }
-                    _ => {}
-                }
-            }
-        });
-    };
-
     ($service_name:ident, $service_func_name:ident, $define_handler:expr, $on_stop:expr, $service_main_func:ident) => {
         $crate::paste::paste! {
             pub fn [<$service_func_name _main>]() -> u32 {
                 let (sender, receiver) = $define_handler;
                 let sender_ = sender.clone();
 
-                #[cfg(feature = "tokio-signals")]
+
+
+                #[cfg(feature = "tokio")]
                 {
-                    define_service!(@tokio_signal_handler $on_stop(&sender_));
+                    $crate::tokio::spawn(async move {
+                        use $crate::futures::stream::StreamExt;
+
+                        let signals = $crate::signal_hook_tokio::Signals::new(&[
+                            $crate::signal_hook::consts::signal::SIGHUP,
+                            $crate::signal_hook::consts::signal::SIGTERM,
+                            $crate::signal_hook::consts::signal::SIGINT,
+                            $crate::signal_hook::consts::signal::SIGQUIT,
+                        ])
+                        .unwrap();
+
+                        //let handle = signals.handle();
+
+                        let mut signals = signals.fuse();
+                        while let Some(signal) = signals.next().await {
+                            match signal {
+                                $crate::signal_hook::consts::signal::SIGTERM
+                                | $crate::signal_hook::consts::signal::SIGINT
+                                | $crate::signal_hook::consts::signal::SIGQUIT
+                                | $crate::signal_hook::consts::signal::SIGHUP => {
+                                    $on_stop(&sender_);
+                                }
+                                _ => {}
+                            }
+                        }
+                    });
                 }
-                #[cfg(not(feature="tokio-signals"))]
+                #[cfg(not(feature="tokio"))]
                 {
-                    define_service!(@std_signal_handler $on_stop(&sender_));
+                    std::thread::spawn(move || {
+                        let mut signals = $crate::signal_hook::iterator::Signals::new(&[
+                            $crate::signal_hook::consts::signal::SIGHUP,
+                            $crate::signal_hook::consts::signal::SIGTERM,
+                            $crate::signal_hook::consts::signal::SIGINT,
+                            $crate::signal_hook::consts::signal::SIGQUIT,
+                        ])
+                        .unwrap();
+
+                        for signal in &mut signals {
+                            match signal {
+                                $crate::signal_hook::consts::signal::SIGTERM
+                                | $crate::signal_hook::consts::signal::SIGINT
+                                | $crate::signal_hook::consts::signal::SIGQUIT
+                                | $crate::signal_hook::consts::signal::SIGHUP => {
+                                    $on_stop(&sender_);
+                                }
+                                _ => {}
+                            }
+                        }
+                    });
+
                 }
 
                 $service_main_func(sender, receiver)
