d86f46347a9c17882eb8adef7942a39a729e9e3b
Austin Schey <aschey13@gmail.com>
2022-08-22 19:57:16 -0500

    remove extra tokio features

--- src/bin/bin_fixture.rs
+++ src/bin/bin_fixture.rs
@@ -40,37 +40,39 @@ pub fn main() {
 }
 
 #[cfg(feature = "async-tokio")]
-#[tokio::main]
-pub async fn main() {
-    let config = ServiceConfig::new(Handler::get_service_name())
-        .with_description("test service")
-        .with_args(["-r"]);
-    let manager = Manager::new(config).unwrap();
-    let args: Vec<String> = std::env::args().collect();
-    let arg = if args.len() > 1 { &args[1] } else { "" };
-    match arg {
-        "-i" => {
-            manager.install().unwrap();
-            manager.start().unwrap();
+pub fn main() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let config = ServiceConfig::new(Handler::get_service_name())
+            .with_description("test service")
+            .with_args(["-r"]);
+        let manager = Manager::new(config).unwrap();
+        let args: Vec<String> = std::env::args().collect();
+        let arg = if args.len() > 1 { &args[1] } else { "" };
+        match arg {
+            "-i" => {
+                manager.install().unwrap();
+                manager.start().unwrap();
+            }
+            "-s" => {
+                manager.start().unwrap();
+            }
+            "-h" => {
+                manager.stop().unwrap();
+            }
+            "-u" => {
+                manager.stop().unwrap();
+                manager.uninstall().unwrap();
+            }
+            "-r" => {
+                run_service().await;
+            }
+            _ => {
+                #[cfg(feature = "direct")]
+                run_service_main().await;
+            }
         }
-        "-s" => {
-            manager.start().unwrap();
-        }
-        "-h" => {
-            manager.stop().unwrap();
-        }
-        "-u" => {
-            manager.stop().unwrap();
-            manager.uninstall().unwrap();
-        }
-        "-r" => {
-            run_service().await;
-        }
-        _ => {
-            #[cfg(feature = "direct")]
-            run_service_main().await;
-        }
-    }
+    });
 }
 
 define_service!(run_service, Handler);
@@ -110,14 +112,15 @@ mod app {
 mod app {
     use async_trait::async_trait;
     use daemon_slayer::service_manager::{ServiceHandler, StopHandler};
+    use futures::{SinkExt, StreamExt};
     pub struct Handler {
-        tx: tokio::sync::mpsc::Sender<()>,
-        rx: tokio::sync::mpsc::Receiver<()>,
+        tx: futures::channel::mpsc::Sender<()>,
+        rx: futures::channel::mpsc::Receiver<()>,
     }
     #[async_trait]
     impl ServiceHandler for Handler {
         fn new() -> Self {
-            let (tx, rx) = tokio::sync::mpsc::channel(32);
+            let (tx, rx) = futures::channel::mpsc::channel(32);
             Self { tx, rx }
         }
         fn get_service_name<'a>() -> &'a str {
@@ -127,13 +130,13 @@ mod app {
         fn get_stop_handler(&mut self) -> StopHandler {
             let tx = self.tx.clone();
             Box::new(move || {
-                let tx = tx.clone();
+                let mut tx = tx.clone();
                 Box::pin(async move { tx.send(()).await.unwrap() })
             })
         }
 
         async fn run_service(&mut self) -> u32 {
-            self.rx.recv().await.unwrap();
+            self.rx.next().await;
             0
         }
     }
