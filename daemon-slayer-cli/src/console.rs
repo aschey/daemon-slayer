95d56bbde6b63f590c6138f55db124b0ad9e5b29
Austin Schey <aschey13@gmail.com>
2022-09-02 22:40:52 -0500

    add logging and controls

--- daemon-slayer-cli/src/console.rs
+++ daemon-slayer-cli/src/console.rs
@@ -8,7 +8,8 @@ use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
-    widgets::{Block, BorderType, Borders, Paragraph},
+    text::{Span, Spans},
+    widgets::{Block, BorderType, Borders, Paragraph, Wrap},
     Frame, Terminal,
 };
 
@@ -55,30 +56,20 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
     let size = f.size();
 
     // Main border
-    let main_block = Block::default()
-        .borders(Borders::ALL)
-        .title("Daemon Slayer Console")
-        .title_alignment(Alignment::Center)
-        .border_type(BorderType::Rounded);
+    let main_block = get_main_block();
     f.render_widget(main_block, size);
 
-    let sides = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-        .vertical_margin(1)
-        .horizontal_margin(2)
-        .split(f.size());
-    let left = sides[0];
+    let (left, right) = get_left_right(size);
 
     let num_labels = 3;
-    let left_side = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Length(3 * num_labels), Constraint::Min(1)])
-        .split(left);
+    let (label_section, info_section) = get_left_sections(left, num_labels);
+
     let status_area = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Length(25), Constraint::Min(1)])
-        .split(left_side[0]);
+        .split(label_section);
+
+    let status_block = bordered_block().border_style(reset_all());
 
     let label_area = get_label_area(num_labels, left);
 
@@ -91,18 +82,71 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
     let health_check_label = get_label("Health:");
     let health_check_value = get_label_value("Healthy", Color::Green);
 
+    let text = vec![
+        Spans::from(Span::raw("Event Viewer: Daemon Slayer Test Service")),
+        Spans::from(Span::raw("Log file: C:\\\\Users\\bob\\test\\dir")),
+    ];
+    let logging_paragraph = Paragraph::new(text)
+        .block(
+            Block::default()
+                .style(reset_all())
+                .title(Spans::from(Span::styled(
+                    "Log Sources",
+                    Style::default().add_modifier(Modifier::UNDERLINED),
+                ))),
+        )
+        .wrap(Wrap { trim: false });
+
     f.render_widget(status_label, label_area.0[0]);
     f.render_widget(status_value, label_area.1[0]);
     f.render_widget(autostart_label, label_area.0[1]);
     f.render_widget(autostart_value, label_area.1[1]);
     f.render_widget(health_check_label, label_area.0[2]);
     f.render_widget(health_check_value, label_area.1[2]);
-
-    let status_block = Block::default()
-        .borders(Borders::ALL)
-        .border_style(Style::default().fg(Color::Reset))
-        .border_type(BorderType::Rounded);
+    //f.render_widget(logging_label, log_section[0]);
+    f.render_widget(logging_paragraph, info_section);
     f.render_widget(status_block, status_area[0]);
+
+    let button = Paragraph::new(Spans::from(Span::styled(
+        " start ",
+        Style::default().bg(Color::Green),
+    )))
+    .block(bordered_block().title("Controls"));
+    f.render_widget(button, right);
+}
+
+fn get_main_block() -> Block<'static> {
+    bordered_block()
+        .title("Daemon Slayer Console")
+        .title_alignment(Alignment::Center)
+}
+
+fn bordered_block() -> Block<'static> {
+    Block::default()
+        .borders(Borders::all())
+        .border_type(BorderType::Rounded)
+}
+
+fn get_left_right(parent: Rect) -> (Rect, Rect) {
+    let sides = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .vertical_margin(1)
+        .horizontal_margin(2)
+        .split(parent);
+    (sides[0], sides[1])
+}
+
+fn get_left_sections(left: Rect, num_labels: u16) -> (Rect, Rect) {
+    let sections = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(3 * num_labels), Constraint::Min(1)])
+        .split(left);
+    let info_section = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Length(1), Constraint::Min(1)])
+        .split(sections[1]);
+    (sections[0], info_section[1])
 }
 
 fn get_label(label: &str) -> Paragraph {
@@ -135,4 +179,11 @@ fn get_label_area(num_labels: u16, left: Rect) -> (Vec<Rect>, Vec<Rect>) {
         .split(vert_slices[1]);
 
     (labels, values)
+}
+
+fn reset_all() -> Style {
+    Style::default()
+        .fg(Color::Reset)
+        .bg(Color::Reset)
+        .remove_modifier(Modifier::all())
 }
