b2ad5f9476ed613188a5ae5c7b9f4fc49f8e6f52
Austin Schey <aschey13@gmail.com>
2022-10-20 21:46:28 -0500

    handle start/stop/restart

--- daemon-slayer-tray/src/lib.rs
+++ daemon-slayer-tray/src/lib.rs
@@ -1,4 +1,6 @@
-use daemon_slayer_client::{Manager, ServiceManager};
+use std::time::{Duration, Instant};
+
+use daemon_slayer_client::{Manager, ServiceManager, State};
 #[cfg(target_os = "macos")]
 use tao::platform::macos::{SystemTrayBuilderExtMacOS, SystemTrayExtMacOS};
 use tao::{
@@ -11,12 +13,14 @@ use tao::{
 
 pub fn start(icon_path: &std::path::Path, manager: ServiceManager) {
     let event_loop = EventLoop::new();
+    let current_state = manager.info().unwrap().state;
+    let start_stop_text = get_start_stop_text(&current_state);
 
     let main_tray_id = TrayId::new("main-tray");
     let icon = load_icon(icon_path);
     let mut tray_menu = Menu::new();
-    let start_item = tray_menu.add_item(MenuItemAttributes::new("Start"));
-    let stop_item = tray_menu.add_item(MenuItemAttributes::new("Stop"));
+    let mut start_stop_item = tray_menu.add_item(MenuItemAttributes::new(start_stop_text));
+    let restart_item = tray_menu.add_item(MenuItemAttributes::new("Restart"));
     let quit_item = tray_menu.add_item(MenuItemAttributes::new("Quit"));
 
     let mut system_tray = Some(
@@ -30,8 +34,14 @@ pub fn start(icon_path: &std::path::Path, manager: ServiceManager) {
     let system_tray = system_tray.with_title("Tao");
 
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+        *control_flow = ControlFlow::WaitUntil(
+            Instant::now()
+                .checked_add(Duration::from_millis(1000))
+                .unwrap(),
+        );
 
+        let current_state = manager.info().unwrap().state;
+        start_stop_item.set_title(get_start_stop_text(&current_state));
         if let Event::MenuEvent {
             menu_id,
             origin: MenuType::ContextMenu,
@@ -41,13 +51,25 @@ pub fn start(icon_path: &std::path::Path, manager: ServiceManager) {
             if menu_id == quit_item.clone().id() {
                 system_tray.take();
                 *control_flow = ControlFlow::Exit;
-            } else if menu_id == start_item.clone().id() {
-                manager.start().unwrap();
-            } else if menu_id == stop_item.clone().id() {
-                manager.stop().unwrap();
+            } else if menu_id == start_stop_item.clone().id() {
+                if current_state == State::Started {
+                    manager.stop().unwrap();
+                } else {
+                    manager.start().unwrap();
+                }
+            } else if menu_id == restart_item.clone().id() {
+                manager.restart().unwrap();
             }
         }
     });
+}
+
+fn get_start_stop_text(state: &State) -> &str {
+    if state == &State::Started {
+        "Stop"
+    } else {
+        "Start"
+    }
 }
 
 fn load_icon(path: &std::path::Path) -> tao::system_tray::Icon {
