ba1c1b5ac9b66f1ea7783417b3c3582cccf09991
Austin Schey <aschey13@gmail.com>
2022-10-21 23:32:43 -0500

    add commands

--- daemon-slayer-gui/src-tauri/src/main.rs
+++ daemon-slayer-gui/src-tauri/src/main.rs
@@ -1,6 +1,17 @@
-use daemon_slayer_client::{Manager, ServiceManager, State};
-use tauri::{CustomMenuItem, Manager as TauriManager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use std::{sync::Arc, time::Duration};
+
+use daemon_slayer_client::{Manager, ServiceManager, State as ServiceState};
+use tauri::{
+    CustomMenuItem, Manager as TauriManager, State, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    WindowEvent,
+};
 use tauri_plugin_positioner::{on_tray_event, Position, WindowExt};
+
+#[derive(Clone, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Payload {
+    service_state: String,
+}
 
 fn main() {
     let manager = ServiceManager::new("daemon_slayer_axum").unwrap();
@@ -16,10 +27,43 @@ fn main() {
         .add_item(tray_quit);
 
     let system_tray = SystemTray::new().with_menu(tray_menu);
-
+    let manager_ = manager.clone();
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![toggle, get_service_state])
         .plugin(tauri_plugin_positioner::init())
         .system_tray(system_tray)
+        .setup(move |app| {
+            let win = app.get_window("main").unwrap();
+            tauri::async_runtime::spawn(async move {
+                let mut state = manager_.info().unwrap().state;
+                loop {
+                    let new_state = manager_.info().unwrap().state;
+                    if new_state != state {
+                        state = new_state;
+                        win.emit(
+                            "service_state",
+                            Payload {
+                                service_state: match state {
+                                    ServiceState::Started => "started".to_string(),
+                                    ServiceState::Stopped => "stopped".to_string(),
+                                    ServiceState::NotInstalled => "not_installed".to_string(),
+                                },
+                            },
+                        )
+                        .unwrap();
+                    }
+
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                }
+            });
+            Ok(())
+        })
+        .manage(manager.clone())
+        .on_window_event(|event| {
+            if let WindowEvent::Focused(false) = event.event() {
+                event.window().hide().unwrap();
+            }
+        })
         .on_system_tray_event(move |app, event| {
             let state = manager.info().unwrap().state;
             app.tray_handle()
@@ -39,7 +83,7 @@ fn main() {
             if let SystemTrayEvent::MenuItemClick { id, .. } = event {
                 match id.as_str() {
                     "start_stop" => {
-                        if state == State::Started {
+                        if state == ServiceState::Started {
                             manager.stop().unwrap();
                         } else {
                             manager.start().unwrap();
@@ -59,8 +103,27 @@ fn main() {
         .expect("failed to run app");
 }
 
-fn get_start_stop_text(state: &State) -> &str {
-    if state == &State::Started {
+#[tauri::command]
+fn toggle(state: String, manager: State<ServiceManager>) {
+    if state == "started" {
+        manager.stop().unwrap();
+    } else {
+        manager.start().unwrap();
+    }
+}
+
+#[tauri::command]
+fn get_service_state(manager: State<ServiceManager>) -> String {
+    let state = manager.info().unwrap().state;
+    match state {
+        ServiceState::Started => "started".to_string(),
+        ServiceState::Stopped => "stopped".to_string(),
+        ServiceState::NotInstalled => "not_installed".to_string(),
+    }
+}
+
+fn get_start_stop_text(state: &ServiceState) -> &str {
+    if state == &ServiceState::Started {
         "Stop"
     } else {
         "Start"
--- daemon-slayer-gui/src/pages/index.tsx
+++ daemon-slayer-gui/src/pages/index.tsx
@@ -1,8 +1,30 @@
 import { AppProps } from 'next/app';
 import { ColorScheme, Button } from '@mantine/core';
+import { invoke } from '@tauri-apps/api/tauri';
+import { useEffect, useState } from 'react';
+import { listen } from '@tauri-apps/api/event';
 
 const Index = (props: AppProps & { colorScheme: ColorScheme }) => {
-  return <Button>Settings</Button>;
+  const [serviceState, setServiceState] = useState('');
+  useEffect(() => {
+    invoke<string>('get_service_state').then(setServiceState);
+    const unlistenPromise = listen<{ serviceState: string }>(
+      'service_state',
+      (event) => setServiceState(event.payload.serviceState)
+    );
+    return () => {
+      unlistenPromise.then((unlisten) => unlisten());
+    };
+  }, []);
+
+  const getButtonText = () => {
+    return serviceState === 'started' ? 'Stop' : 'Start';
+  };
+  return (
+    <Button onClick={() => invoke('toggle', { state: serviceState })}>
+      {getButtonText()}
+    </Button>
+  );
 };
 
 export default Index;
