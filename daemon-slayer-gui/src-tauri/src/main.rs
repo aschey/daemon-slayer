2b7b8e8afbfadbd912d0ef9337a89c8b845c329d
Austin Schey <aschey13@gmail.com>
2022-10-21 22:16:08 -0500

    basic ui

--- daemon-slayer-gui/src-tauri/src/main.rs
+++ daemon-slayer-gui/src-tauri/src/main.rs
@@ -1,48 +1,68 @@
-use std::thread;
-
-use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use daemon_slayer_client::{Manager, ServiceManager, State};
+use tauri::{CustomMenuItem, Manager as TauriManager, SystemTray, SystemTrayEvent, SystemTrayMenu};
 use tauri_plugin_positioner::{on_tray_event, Position, WindowExt};
 
 fn main() {
-    let tray_start = CustomMenuItem::new("start".to_string(), "Start");
-    let tray_stop = CustomMenuItem::new("stop".to_string(), "Stop");
+    let manager = ServiceManager::new("daemon_slayer_axum").unwrap();
+    let state = manager.info().unwrap().state;
+    let tray_start_stop =
+        CustomMenuItem::new("start_stop".to_string(), get_start_stop_text(&state));
+    let tray_restart = CustomMenuItem::new("restart".to_string(), "Restart");
     let tray_quit = CustomMenuItem::new("quit".to_string(), "Quit");
 
     let tray_menu = SystemTrayMenu::new()
-        .add_item(tray_quit)
-        .add_item(tray_start)
-        .add_item(tray_stop);
+        .add_item(tray_start_stop)
+        .add_item(tray_restart)
+        .add_item(tray_quit);
 
     let system_tray = SystemTray::new().with_menu(tray_menu);
 
     tauri::Builder::default()
         .plugin(tauri_plugin_positioner::init())
         .system_tray(system_tray)
-        .on_system_tray_event(|app, event| {
+        .on_system_tray_event(move |app, event| {
+            let state = manager.info().unwrap().state;
+            app.tray_handle()
+                .get_item("start_stop")
+                .set_title(get_start_stop_text(&state))
+                .unwrap();
             on_tray_event(app, &event);
             let win = app.get_window("main").unwrap();
-            if let SystemTrayEvent::LeftClick { position, size, .. } = event {
+            if let SystemTrayEvent::LeftClick { .. } = event {
                 if win.is_visible().unwrap() {
-                    win.hide();
+                    win.hide().unwrap();
                 } else {
-                    win.move_window(Position::TrayCenter);
-                    win.show();
+                    win.move_window(Position::TrayCenter).unwrap();
+                    win.show().unwrap();
                 }
             }
             if let SystemTrayEvent::MenuItemClick { id, .. } = event {
                 match id.as_str() {
+                    "start_stop" => {
+                        if state == State::Started {
+                            manager.stop().unwrap();
+                        } else {
+                            manager.start().unwrap();
+                        }
+                    }
+                    "restart" => {
+                        manager.restart().unwrap();
+                    }
                     "quit" => {
                         app.exit(0);
                     }
-                    // "tray_bottom_left" => win.move_window(Position::TrayBottomLeft),
-                    // "tray_right" => win.move_window(Position::TrayRight),
-                    // "tray_bottom_right" => win.move_window(Position::TrayBottomRight),
-                    // "tray_center" => win.move_window(Position::TrayCenter),
-                    // "tray_bottom_center" => win.move_window(Position::TrayBottomCenter),
                     _ => {}
                 };
             }
         })
         .run(tauri::generate_context!())
         .expect("failed to run app");
+}
+
+fn get_start_stop_text(state: &State) -> &str {
+    if state == &State::Started {
+        "Stop"
+    } else {
+        "Start"
+    }
 }
--- daemon-slayer-gui/src/pages/_app.tsx
+++ daemon-slayer-gui/src/pages/_app.tsx
@@ -1,9 +1,57 @@
-import type { AppProps } from "next/app";
+import { GetServerSidePropsContext } from 'next';
+import { useState } from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+} from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
-import "../style.css";
-import "../App.css";
+const App = (props: AppProps & { colorScheme: ColorScheme }) => {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Mantine next example</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+        <link rel='shortcut icon' href='/favicon.svg' />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <NotificationsProvider>
+            <Component {...pageProps} />
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
+};
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: 'dark',
+});
+
+export default App;
