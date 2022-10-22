20851c9829a407048d0ba275aae549562cb98f46
Austin Schey <aschey13@gmail.com>
2022-10-21 19:11:45 -0500

    add tray

--- daemon-slayer-gui/src-tauri/src/main.rs
+++ daemon-slayer-gui/src-tauri/src/main.rs
@@ -1,17 +1,48 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+use std::thread;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri_plugin_positioner::{on_tray_event, Position, WindowExt};
 
 fn main() {
+    let tray_start = CustomMenuItem::new("start".to_string(), "Start");
+    let tray_stop = CustomMenuItem::new("stop".to_string(), "Stop");
+    let tray_quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(tray_quit)
+        .add_item(tray_start)
+        .add_item(tray_stop);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| {
+            on_tray_event(app, &event);
+            let win = app.get_window("main").unwrap();
+            if let SystemTrayEvent::LeftClick { position, size, .. } = event {
+                if win.is_visible().unwrap() {
+                    win.hide();
+                } else {
+                    win.move_window(Position::TrayCenter);
+                    win.show();
+                }
+            }
+            if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+                match id.as_str() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    // "tray_bottom_left" => win.move_window(Position::TrayBottomLeft),
+                    // "tray_right" => win.move_window(Position::TrayRight),
+                    // "tray_bottom_right" => win.move_window(Position::TrayBottomRight),
+                    // "tray_center" => win.move_window(Position::TrayCenter),
+                    // "tray_bottom_center" => win.move_window(Position::TrayBottomCenter),
+                    _ => {}
+                };
+            }
+        })
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("failed to run app");
 }
