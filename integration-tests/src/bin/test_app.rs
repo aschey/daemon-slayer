708de4d850adb08db255d12ca822e916b7abe858
Austin Schey <aschey13@gmail.com>
2022-12-25 15:56:19 -0600

    test user and system services

--- integration-tests/src/bin/test_app.rs
+++ integration-tests/src/bin/test_app.rs
@@ -3,6 +3,7 @@ use axum::Router;
 use daemon_slayer::cli::Cli;
 use daemon_slayer::client;
 use daemon_slayer::client::cli::ClientCliProvider;
+use daemon_slayer::client::config::Level;
 use daemon_slayer::core::{BoxedError, Label};
 use daemon_slayer::error_handler::cli::ErrorHandlerCliProvider;
 use daemon_slayer::logging::cli::LoggingCliProvider;
@@ -22,12 +23,18 @@ use daemon_slayer::logging::tracing_subscriber::util::SubscriberInitExt;
 #[tokio::main]
 pub async fn main() {
     let run_argument = "-r".parse().unwrap();
-    let manager_builder = client::builder(
+    let mut manager_builder = client::builder(
         ServiceHandler::label(),
         current_exe().unwrap().try_into().unwrap(),
     )
     .with_description("test service")
     .with_arg(&run_argument);
+
+    if let Ok(user_service) = std::env::var("USER_SERVICE") {
+        if user_service == "1" || user_service == "true" {
+            manager_builder = manager_builder.with_service_level(Level::User);
+        }
+    }
 
     let manager = manager_builder.build().unwrap();
     let logger_builder = LoggerBuilder::new(ServiceHandler::label());
--- integration-tests/tests/service_test.rs
+++ integration-tests/tests/service_test.rs
@@ -1,16 +1,29 @@
 use assert_cmd::Command;
-use daemon_slayer::client;
 use daemon_slayer::client::State;
+use daemon_slayer::client::{self, config::Level};
 use std::{thread, time::Duration};
 
 #[test]
-fn test_combined() {
+fn run() {
+    if cfg!(windows) || std::env::var("RUN_AS_SYSTEM") == Ok("true".to_owned()) {
+        run_tests(false);
+    } else {
+        run_tests(true);
+    }
+}
+
+fn run_tests(is_user_service: bool) {
     let bin_name = "test_app";
     let port = 3002;
     let manager = client::config::Builder::new(
         "com.test.daemon_slayer_test".parse().unwrap(),
         assert_cmd::cargo::cargo_bin(bin_name).try_into().unwrap(),
     )
+    .with_service_level(if is_user_service {
+        Level::User
+    } else {
+        Level::System
+    })
     .build()
     .unwrap();
     if manager.info().unwrap().state != State::NotInstalled {
@@ -25,19 +38,19 @@ fn test_combined() {
         });
     }
 
-    run_manager_cmd(bin_name, "install", || {
+    run_manager_cmd(bin_name, "install", is_user_service, || {
         let info = manager.info().unwrap();
         println!("Waiting for install: {info:?}");
         info.state == State::Stopped && info.autostart == Some(false) && info.pid.is_none()
     });
 
-    run_manager_cmd(bin_name, "start", || {
+    run_manager_cmd(bin_name, "start", is_user_service, || {
         let info = manager.info().unwrap();
         println!("Waiting for start: {info:?}");
         info.state == State::Started && info.autostart == Some(false) && info.pid.is_some()
     });
 
-    run_manager_cmd(bin_name, "enable", || {
+    run_manager_cmd(bin_name, "enable", is_user_service, || {
         let autostart = manager.info().unwrap().autostart.unwrap();
         println!("Waiting for autostart: {autostart:?}");
         autostart
@@ -52,19 +65,19 @@ fn test_combined() {
         false
     });
 
-    run_manager_cmd(bin_name, "disable", || {
+    run_manager_cmd(bin_name, "disable", is_user_service, || {
         let autostart = manager.info().unwrap().autostart.unwrap();
         println!("Waiting for autostart disable: {autostart:?}");
         !autostart
     });
 
-    run_manager_cmd(bin_name, "stop", || {
+    run_manager_cmd(bin_name, "stop", is_user_service, || {
         let info = manager.info().unwrap();
         println!("Waiting for stop: {info:?}");
         info.state == State::Stopped && info.autostart == Some(false) && info.pid.is_none()
     });
 
-    run_manager_cmd(bin_name, "uninstall", || {
+    run_manager_cmd(bin_name, "uninstall", is_user_service, || {
         let info = manager.info().unwrap();
         println!("Waiting for uninstall: {info:?}");
         info.state == State::NotInstalled && info.autostart.is_none() && info.pid.is_none()
@@ -77,10 +90,11 @@ fn wait() {
     thread::sleep(Duration::from_millis(100));
 }
 
-fn run_manager_cmd(bin_name: &str, cmd: &str, condition: impl Fn() -> bool) {
+fn run_manager_cmd(bin_name: &str, cmd: &str, is_user_service: bool, condition: impl Fn() -> bool) {
     Command::cargo_bin(bin_name)
         .unwrap()
         .arg(cmd)
+        .env("USER_SERVICE", is_user_service.to_string())
         .output()
         .unwrap();
     wait_for(condition);
