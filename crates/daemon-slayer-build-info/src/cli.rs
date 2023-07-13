06b0048693a48a28ba887081692d5aa0a67135ca
Austin Schey <aschey13@gmail.com>
2023-07-13 15:47:55 -0500

    fix command handling

--- crates/daemon-slayer-build-info/src/cli.rs
+++ crates/daemon-slayer-build-info/src/cli.rs
@@ -31,7 +31,10 @@ impl CommandProvider for BuildInfoCliProvider {
     }
 
     fn matches(&self, matches: &clap::ArgMatches) -> Option<CommandMatch> {
-        CliArgs::from_arg_matches(matches).ok()?;
+        let cli_args = CliArgs::from_arg_matches(matches).ok()?;
+        if !cli_args.build_info || matches.subcommand().is_some() {
+            return None;
+        }
         Some(CommandMatch {
             action_type: ActionType::Other,
             action: None,
@@ -43,14 +46,14 @@ impl CommandProvider for BuildInfoCliProvider {
         matches: &clap::ArgMatches,
         _matched_command: &Option<CommandMatch>,
     ) -> Result<CommandOutput, BoxedError> {
-        match CliArgs::from_arg_matches(matches) {
-            Ok(_) => {
+        match (matches.subcommand(), CliArgs::from_arg_matches(matches)) {
+            (None, Ok(CliArgs { build_info: true })) => {
                 let mut buf = Vec::new();
                 self.output.display(&mut buf).unwrap();
 
                 Ok(CommandOutput::handled(String::from_utf8(buf).unwrap()))
             }
-            Err(_) => Ok(CommandOutput::unhandled()),
+            _ => Ok(CommandOutput::unhandled()),
         }
     }
 }
