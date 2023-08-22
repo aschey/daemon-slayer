eed52f299477af147ff841c53f40964e6100a9e5
Austin Schey <aschey13@gmail.com>
2023-08-21 23:44:04 -0500

    fix return type

--- crates/daemon-slayer-server/src/socket_activation/windows.rs
+++ crates/daemon-slayer-server/src/socket_activation/windows.rs
@@ -1,10 +1,12 @@
 use daemon_slayer_core::socket_activation::ActivationSocketConfig;
 
-use super::{create_sockets, to_hash_map, SocketResult};
+use super::{create_sockets, to_hash_map, SocketActivationResult, SocketResult};
 
-pub async fn get_activation_sockets(socket_config: Vec<ActivationSocketConfig>) -> SocketResult {
+pub async fn get_activation_sockets(
+    socket_config: Vec<ActivationSocketConfig>,
+) -> SocketActivationResult {
     let sockets = create_sockets(socket_config).await;
-    SocketResult {
+    SocketActivationResult {
         sockets: to_hash_map(sockets),
         is_activated: false,
     }
